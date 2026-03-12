e234d9da401df8eb0a1c6522bdf132bdde82af88
Robert <barrosor@oregonstate.edu>
2026-03-12 17:04:41 -0400

    Working on REST API calls. DONE: POST, GET. WORKING: PUT, DELETE

--- exercises_controller.mjs
+++ exercises_controller.mjs
@@ -1,17 +1,105 @@
 /**
- * Add your first name and last name.
+ * Robert Barroso
  */
-import 'dotenv/config';
-import express from 'express';
-import asyncHandler from 'express-async-handler';
-import * as exercises from './exercises_model.mjs';
+import "dotenv/config";
+import express from "express";
+import asyncHandler from "express-async-handler";
+import * as exercises from "./exercises_model.mjs";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+function isDateValid(date) {
+  const format = /^\d\d-\d\d-\d\d$/;
+  return format.test(date);
+}
+
+const ERROR_INVALID_REQUEST = { Error: "Invalid request" };
+const ERROR_NOT_FOUND = { Error: "Not found" };
 
 app.use(express.json());
 
 app.listen(PORT, async () => {
-    await exercises.connect()
-    console.log(`Server listening on port ${PORT}...`);
+  await exercises.connect();
+  console.log(`Server listening on port ${PORT}...`);
 });
+
+function validateInput(req) {
+  if (
+    req.body.name === undefined ||
+    req.body.reps === undefined ||
+    req.body.weight === undefined ||
+    req.body.unit === undefined ||
+    req.body.date === undefined ||
+    req.body.name.length <= 0 ||
+    req.body.reps <= 0 ||
+    !Number.isInteger(req.body.reps) ||
+    req.body.weight <= 0 ||
+    !Number.isInteger(req.body.weight) ||
+    (req.body.unit !== "kgs" && req.body.unit !== "lbs") ||
+    isDateValid(req.body.date) === false
+  ) {
+    return false;
+  }
+  return true;
+}
+
+// POST /exercises
+app.post(
+  "/exercises",
+  asyncHandler(async (req, res) => {
+    if (!validateInput(req)) {
+      res.status(400).json(ERROR_INVALID_REQUEST);
+    }
+    const newExercise = await exercises.createExercise(
+      req.body.name,
+      req.body.reps,
+      req.body.weight,
+      req.body.unit,
+      req.body.date,
+    );
+
+    res.status(201).json(newExercise);
+  }),
+);
+
+// GET /exercises
+app.get(
+  "/exercises",
+  asyncHandler(async (req, res) => {
+    const resultExercises = await exercises.getExercises(req.query);
+    return res.status(200).json(resultExercises);
+  }),
+);
+
+// GET /exercises/:_id
+app.get(
+  "/exercises/:id",
+  asyncHandler(async (req, res) => {
+    const resultExercise = await exercises.getExercise(req.params.id);
+    if (!resultExercise) {
+      res.status(404).json(ERROR_NOT_FOUND);
+    } else {
+      res.status(200).json(resultExercise);
+    }
+  }),
+);
+
+// PUT /exercises/:_id
+app.put(
+  "/exercises/:id",
+  asyncHandler(async (req, res) => {
+    if (!validateInput(req)) {
+      res.status(400).json(ERROR_INVALID_REQUEST);
+    }
+    const resultExercise = await exercises.updateExercise(
+      req.params.id,
+      req.body,
+    );
+    if (!resultExercise) {
+      res.status(404).json(ERROR_NOT_FOUND);
+    } else {
+      res.status(200).json(resultExercise);
+    }
+  }),
+);
--- exercises_model.mjs
+++ exercises_model.mjs
@@ -1,10 +1,11 @@
 /**
  * Add your first name and last name.
  */
-import mongoose from 'mongoose';
-import 'dotenv/config';
+import mongoose, { mongo } from "mongoose";
+import "dotenv/config";
 
-const EXERCISE_DB_NAME = 'exercise_db';
+const EXERCISE_DB_NAME = "exercise_db";
+const EXERCISE_CLASS = "Exercise";
 
 let connection = undefined;
 
@@ -12,15 +13,54 @@ let connection = undefined;
  * This function connects to the MongoDB server and to the database
  *  'exercise_db' in that server.
  */
-async function connect(){
-    try{
-        connection = await mongoose.connect(process.env.MONGODB_CONNECT_STRING, 
-                {dbName: EXERCISE_DB_NAME});
-        console.log("Successfully connected to MongoDB using Mongoose!");
-    } catch(err){
-        console.log(err);
-        throw Error(`Could not connect to MongoDB ${err.message}`)
-    }
+async function connect() {
+  try {
+    connection = await mongoose.connect(process.env.MONGODB_CONNECT_STRING, {
+      dbName: EXERCISE_DB_NAME,
+    });
+    console.log("Successfully connected to MongoDB using Mongoose!");
+  } catch (err) {
+    console.log(err);
+    throw Error(`Could not connect to MongoDB ${err.message}`);
+  }
 }
 
-export { connect};
+const exerciseSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  reps: { type: Number, required: true },
+  weight: { type: Number, required: true },
+  unit: { type: String, required: true },
+  date: { type: String, required: true },
+});
+
+const Exercise = mongoose.model(EXERCISE_CLASS, exerciseSchema);
+
+const createExercise = async (name, reps, weight, unit, date) => {
+  const newExercise = new Exercise({
+    name: name,
+    reps: reps,
+    weight: weight,
+    unit: unit,
+    date: date,
+  });
+  return newExercise.save();
+};
+
+const getExercises = async (filter) => {
+  const query = Exercise.find(filter);
+  return query.exec();
+};
+
+const getExercise = async (filter) => {
+  const query = Exercise.findById(filter);
+  return query.exec();
+};
+
+const updateExercise = async (filter, resBody) => {
+  const query = await Exercise.findByIdAndUpdate(filter, resBody, {
+    new: true,
+  });
+  return query;
+};
+
+export { connect, createExercise, getExercises, getExercise, updateExercise };
